e7c4799d388e0b5d39b76657d68f5e492c98d43d
Lorenzo <lorhernandez@csumb.edu>
2019-12-12 08:51:32 -0800

    added users project

---
+++ users/models.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class Doctor(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+
+    in_office = models.BooleanField(default=False)
